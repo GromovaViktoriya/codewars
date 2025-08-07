a56716f42b99bd744929b624d39015cf0e229185
Viktoriya Gromova <rockingirl@mail.ru>
2025-08-07 18:24:14 +0500

    added 8 kyu AbbreviateTwoWordName

---
+++ 8kyu/AbbreviateTwoWordName.js
@@ -0,0 +1,31 @@
+//DESCRIPTION
+//Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+// The output should be two capital letters with a dot separating them.
+// It should look like this:
+// Sam Harris => S.H
+// patrick feeney => P.F
+
+
+//TESTS
+//     assert.strictEqual(abbrevName("Sam Harris"), "S.H");
+//     assert.strictEqual(abbrevName("Patrick Feenan"), "P.F");
+//     assert.strictEqual(abbrevName("Evan Cole"), "E.C");
+//     assert.strictEqual(abbrevName("P Favuzzi"), "P.F");
+//     assert.strictEqual(abbrevName("David Mendieta"), "D.M");
+
+
+//SOLUTION
+function abbrevName(name) {
+    let namesArr = name.split(' ');
+    let abbrevArr = [];
+    for (let i = 0; i < namesArr.length; i++) {
+        abbrevArr.push(namesArr[i][0]);
+    }
+    return abbrevArr.join('.').toUpperCase();
+}
+
+console.log(abbrevName("Sam Harris"));
+console.log(abbrevName("Patrick Feenan"));
+console.log(abbrevName("Evan Cole"));
+console.log(abbrevName("P Favuzzi"));
+console.log(abbrevName("David Mendieta"))
