332ea859c5e76812c98c369c7c29f94c6d057e8e
Viktoriya Gromova <rockingirl@mail.ru>
2025-08-08 19:48:05 +0500

    solved propertyToEveryObjectInArray 7 kyu

---
+++ 8kyu/propertyToEveryObjectInArray.js
@@ -0,0 +1,51 @@
+//DESCRIPTION
+//Your task is to add a new property usersAnswer to every object in the array questions.
+// The value of usersAnswer should be set to null. The solution should work for array of any length.
+
+//For example:
+//var questions = [{
+//     question: "What's the currency of the USA?",
+//     choices: ["US dollar", "Ruble", "Horses", "Gold"],
+//     corAnswer: 0
+// }, {
+//     question: "Where was the American Declaration of Independence signed?",
+//     choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+//     corAnswer: 0
+// }];
+
+//After adding the property the result should be:
+//
+// var questions = [{
+//     question: "What's the currency of the USA?",
+//     choices: ["US dollar", "Ruble", "Horses", "Gold"],
+//     corAnswer: 0,
+//     usersAnswer: null
+// }, {
+//     question: "Where was the American Declaration of Independence signed?",
+//     choices: ["Philadelphia", "At the bottom", "Frankie's pub", "China"],
+//     corAnswer: 0,
+//     usersAnswer: null
+// }];
+
+//TESTS
+//Test.expect(questions[0].usersAnswer === null);
+
+//SOLUTION
+let questions = [{
+    question: "What's the currency of the USA?",
+    choices: ["US dollar", "Ruble", "Horses", "Gold"],
+    corAnswer: 0
+}, {
+    question: "Where was the American Declaration of Independence signed?",
+    choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+    corAnswer: 0
+}];
+
+function addProperty(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        arr[i].usersAnswer = null;
+    }
+    return arr;
+}
+
+console.log(addProperty(questions));
