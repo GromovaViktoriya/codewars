a41264292873ce89f1aef273e4ca4ffb944695b2
Viktoriya Gromova <rockingirl@mail.ru>
2025-08-17 17:15:36 +0500

    solved disemvowelTrolls in 7kyu

---
+++ 7kyu/disemvowelTrolls.js
@@ -0,0 +1,32 @@
+//DESCRIPTION
+//Trolls are attacking your comment section!
+//
+// A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+//
+// Your task is to write a function that takes a string and return a new string with all vowels removed.
+//
+// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+//
+// Note: for this kata y isn't considered a vowel.
+
+
+//TESTS
+//     assert.strictEqual(disemvowel("This website is for losers LOL!"), "Ths wbst s fr lsrs LL!")
+//     assert.strictEqual(disemvowel("No offense but,\nYour writing is among the worst I've ever read"), "N ffns bt,\nYr wrtng s mng th wrst 'v vr rd")
+//     assert.strictEqual(disemvowel("What are you, a communist?"), "Wht r y,  cmmnst?")
+
+//SOLUTION
+function disemvowel(str) {
+    let vowels = "aeiouAEIOU";
+    let newStr = "";
+    for (let i = 0; i < str.length; i++) {
+        if (!vowels.includes(str[i])) {
+            newStr += str[i];
+        }
+    }
+    return newStr;
+}
+
+console.log(disemvowel("This website is for losers LOL!"))
+console.log(disemvowel("No offense but,\nYour writing is among the worst I've ever read"))
+console.log(disemvowel("What are you, a communist?"))
