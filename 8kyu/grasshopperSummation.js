a1a2cc8b7651d4f3a7db15afcd84f3427581ddad
Viktoriya Gromova <rockingirl@mail.ru>
2025-08-17 18:16:08 +0500

    solved grasshopperSummation 8 kyu

---
+++ 8kyu/grasshopperSummation.js
@@ -0,0 +1,25 @@
+// DESCRIPTION
+//Summation
+// Write a program that finds the summation of every number from 1 to num (both inclusive).
+// The number will always be a positive integer greater than 0. Your function only needs to return the result,
+// what is shown between parentheses in the example below is how you reach that result and it's not part of it,
+// see the sample tests.
+//
+// For example (Input -> Output):
+//
+// 2 -> 3 (1 + 2)
+// 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+
+// TESTS
+//     assert.strictEqual(summation(1),  1);
+//     assert.strictEqual(summation(2),  3);
+//     assert.strictEqual(summation(8), 36);
+
+// SOLUTION
+let summation = function (num) {
+    return num * (num + 1) / 2;
+}
+
+console.log(summation(1));
+console.log(summation(2));
+console.log(summation(8));
