189acb313b272525284b3cd6881ad475671c0c35
Viktoriya Gromova <rockingirl@mail.ru>
2025-08-18 13:03:49 +0500

    solved basicMath 8 kyu

---
+++ 8kyu/basicMath.js
@@ -0,0 +1,38 @@
+// DESCRIPTION
+//Your task is to create a function that does four basic mathematical operations.
+//
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+//
+// Examples(Operator, value1, value2) --> output
+// ('+', 4, 7) --> 11
+// ('-', 15, 18) --> -3
+// ('*', 5, 5) --> 25
+// ('/', 49, 7) --> 7
+
+
+// TESTS
+//     assert.strictEqual(basicOp("+", 4, 7), 11, "4 + 7 = 11");
+//     assert.strictEqual(basicOp("-", 15, 18), -3, "15 - 18 = -3");
+//     assert.strictEqual(basicOp("*", 5, 5), 25, "5 * 5 = 25");
+//     assert.strictEqual(basicOp("/", 49, 7), 7, "49 / 7 = 7");
+
+// SOLUTION
+function basicOp(operation, value1, value2) {
+    let result = 0;
+    if (operation === "+") {
+        result = value1 + value2;
+    } else if (operation === "-") {
+        result = value1 - value2;
+    } else if (operation === "*") {
+        result = value1 * value2;
+    } else if (operation === "/") {
+        result = value1 / value2;
+    }
+    return result
+}
+
+console.log(basicOp("+", 4, 7));
+console.log(basicOp("-", 15, 18));
+console.log(basicOp("*", 5, 5));
+console.log(basicOp("/", 49, 7));
