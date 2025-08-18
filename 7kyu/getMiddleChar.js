9b366b7b43f5ae85d536cc3539b62b59f24aa477
Viktoriya Gromova <rockingirl@mail.ru>
2025-08-18 13:31:14 +0500

    solved toJadenCase in 7kyu

---
+++ 7kyu/getMiddleChar.js
@@ -0,0 +1,33 @@
+//DESCRIPTION
+//You are going to be given a non-empty string. Your job is to return the middle character(s) of the string.
+//
+// If the string's length is odd, return the middle character.
+// If the string's length is even, return the middle 2 characters.
+// Examples:
+// "test" --> "es"
+// "testing" --> "t"
+// "middle" --> "dd"
+// "A" --> "A"
+
+
+//TESTS
+// doTest("test", "es");
+// doTest("testing", "t");
+// doTest("middle", "dd");
+// doTest("A", "A");
+
+
+//SOLUTION
+function getMiddle(s) {
+    const middleIndex = Math.floor(s.length / 2);
+    if (s.length % 2 === 0) {
+        return s.slice(middleIndex - 1, middleIndex + 1);
+    } else {
+        return s[middleIndex];
+    }
+}
+
+console.log(getMiddle("test"))
+console.log(getMiddle("testing"))
+console.log(getMiddle("middle"))
+console.log(getMiddle("A"))
