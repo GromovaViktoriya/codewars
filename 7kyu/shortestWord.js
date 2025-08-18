4e4da05b2249f99bdb4db4cc1cc4daf209b6475f
Viktoriya Gromova <rockingirl@mail.ru>
2025-08-18 17:09:05 +0500

    solved whoIsOnline in 7kyu

--- 7kyu/shortestWord.js
+++ 7kyu/shortestWord.js
@@ -2,6 +2,7 @@
 //Simple, given a string of words, return the length of the shortest word(s).
 //
 // String will never be empty and you do not need to account for different data types.
+
 //TESTS
 //  assert.strictEqual(findShort("bitcoin take over the world maybe who knows perhaps"), 3);
 //     assert.strictEqual(findShort("turns out random test cases are easier than writing out basic ones"), 3);
