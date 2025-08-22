5e12aa60ba57772456912b348a1baee1e2a75b5f
Viktoriya Gromova <rockingirl@mail.ru>
2025-08-22 14:08:05 +0500

    solved stringEndsWith 7kyu

---
+++ 7kyu/stringEndsWith.js
@@ -0,0 +1,22 @@
+//DESCRIPTION
+//Complete the solution so that it returns true if the first argument(string) passed in
+// ends with the 2nd argument (also a string).
+//
+// Examples:
+//
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+
+//TESTS
+// Test.assertEquals(solution('abcde', 'cde'), true)
+// Test.assertEquals(solution('abcde', 'abc'), false)
+
+//SOLUTION
+function solution(str, ending){
+    return str.endsWith(ending);
+
+}
+
+console.log(solution('abcde', 'cde'))
+console.log(solution('abcde', 'abc'))
