a55fce4ff757cec12499c530a670942072e6aae3
Viktoriya Gromova <rockingirl@mail.ru>
2025-09-11 20:45:22 +0500

    solved flattenSortArray  7kyu

---
+++ 7kyu/flattenSortArray.js
@@ -0,0 +1,26 @@
+"use strict";
+// DESCRIPTION
+//
+
+
+
+// TESTS
+//     assert.deepEqual(flattenAndSort([]), []);
+//     assert.deepEqual(flattenAndSort([[], []]), []);
+//     assert.deepEqual(flattenAndSort([[], [1]]), [1]);
+//     assert.deepEqual(flattenAndSort([[3, 2, 1], [7, 9, 8], [6, 4, 5]]), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+//     assert.deepEqual(flattenAndSort([[1, 3, 5], [100], [2, 4, 6]]), [1, 2, 3, 4, 5, 6, 100]);
+
+
+// SOLUTION
+
+
+function flattenAndSort(array) {
+    return array.flat().sort((a, b) => a - b);
+}
+
+console.log(flattenAndSort([]))
+console.log(flattenAndSort([[], []]))
+console.log(flattenAndSort([[], [1]]))
+console.log(flattenAndSort([[3, 2, 1], [7, 9, 8], [6, 4, 5]]))
+console.log(flattenAndSort([[1, 3, 5], [100], [2, 4, 6]]))
