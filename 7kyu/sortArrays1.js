26c0b483181505deddf9fb0de3512e6ef1186687
Viktoriya Gromova <rockingirl@mail.ru>
2025-10-09 21:55:52 +0500

    solved sortArrays1 7 kyu

---
+++ 7kyu/sortArrays1.js
@@ -0,0 +1,17 @@
+// DESCRIPTION
+//Just a simple sorting usage. Create a function that returns the elements of the input-array /
+// list sorted in lexicographical order.
+
+
+// TESTS
+//assert.deepEqual(sortme(['one', 'two', 'three' ]), ["one", "three", "two"])
+
+
+// SOLUTION
+// input: names - unsorted array
+// output: sorted array
+sortme = function( names ){
+    return names.sort()
+}
+
+console.log( sortme(['one', 'two', 'three' ]))
