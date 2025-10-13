c0cf2b4bc89732be1f68685df7dd6cf4bff478cb
Viktoriya Gromova <rockingirl@mail.ru>
2025-10-13 16:43:49 +0500

    solved checkExam 7 kyu

---
+++ 7kyu/maxDiff.js
@@ -0,0 +1,29 @@
+// DESCRIPTION
+// You must implement a function that returns the difference between the largest and the smallest value in a given
+// list / array (lst) received as the parameter.
+//
+// lst contains integers, that means it may contain some negative numbers
+// if lst is empty or contains a single element, return 0
+// lst is not sorted
+// [1, 2, 3, 4]   //  returns 3 because 4 -   1  == 3
+// [1, 2, 3, -4]  //  returns 7 because 3 - (-4) == 7
+
+
+// TESTS
+//     assert.strictEqual(maxDiff([0, 1, 2, 3, 4, 5, 6]), 6);
+//     assert.strictEqual(maxDiff([-0, 1, 2, -3, 4, 5, -6]), 11);
+//     assert.strictEqual(maxDiff([0, 1, 2, 3, 4, 5, 16]), 16);
+//     assert.strictEqual(maxDiff([16]), 0);
+//     assert.strictEqual(maxDiff([]), 0);
+
+
+// SOLUTION
+function maxDiff(list) {
+    return list.length === 1 || list.length === 0? 0 : Math.max(...list) - Math.min(...list)
+}
+
+console.log(maxDiff([0, 1, 2, 3, 4, 5, 6]))
+console.log(maxDiff([-0, 1, 2, -3, 4, 5, -6]))
+console.log(maxDiff([0, 1, 2, 3, 4, 5, 16]))
+console.log(maxDiff([16]))
+console.log(maxDiff([]))
