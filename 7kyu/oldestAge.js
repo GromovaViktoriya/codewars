69b5bec0bcdc78d69e459950ff6b63d794d52193
Viktoriya Gromova <rockingirl@mail.ru>
2025-11-14 16:32:26 +0500

    solved oldestAge 7kyu

---
+++ 7kyu/oldestAge.js
@@ -0,0 +1,34 @@
+// DESCRIPTION
+//The two oldest ages function/method needs to be completed. It should take an array of numbers as its argument and
+// return the two highest numbers within the array. The returned value should be an array in the format
+// [second oldest age,  oldest age].
+//
+// The order of the numbers passed in could be any order. The array will always include at least 2 items. If there are
+// two or more oldest age, then return both of them in array format.
+//
+// For example (Input --> Output):
+//
+// [1, 2, 10, 8] --> [8, 10]
+// [1, 5, 87, 45, 8, 8] --> [45, 87]
+// [1, 3, 10, 0]) --> [3, 10]
+
+
+
+// TESTS
+//     assert.strictEqual(results1[0], 45, 'Passed values of [1,5,87,45,8,8] should return [45, 87]');
+//     assert.strictEqual(results1[1],87, 'Passed values of [1,5,87,45,8,8] should return [45, 87]');
+//     assert.strictEqual(results2[0], 18, 'Passed values of [6,5,83,5,3,18] should return [18, 83]');
+//     assert.strictEqual(results2[1], 83, 'Passed values of [6,5,83,5,3,18] should return [18, 83]');
+
+
+
+// SOLUTION
+// return the two oldest/oldest ages within the array of ages passed in.
+function twoOldestAges(ages){
+return ages.sort((a,b)=> b-a).slice(0,2).reverse()
+}
+
+console.log(twoOldestAges([1, 2, 10, 8]))
+console.log(twoOldestAges([1, 5, 87, 45, 8, 8]))
+console.log(twoOldestAges([6, 5, 83, 5, 3, 18]))
+console.log(twoOldestAges([1, 3, 10, 0]))
