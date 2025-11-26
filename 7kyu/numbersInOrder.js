df3d8d2f56c9b47fb4c64fd02b87e48186c9f671
Viktoriya Gromova <rockingirl@mail.ru>
2025-11-26 14:43:02 +0500

    solved numbersInOrder 7kyu

---
+++ 7kyu/numbersInOrder.js
@@ -0,0 +1,32 @@
+// DESCRIPTION
+//Are the numbers in order?
+// In this Kata, your function receives an array of integers as input. Your task is to determine whether the numbers are
+// in ascending order. An array is said to be in ascending order if there are no two adjacent integers where the left integer exceeds the right integer in value.
+//
+// For the purposes of this Kata, you may assume that all inputs are valid, i.e. arrays containing only integers.
+//
+// Note that an array of 0 or 1 integer(s) is automatically considered to be sorted in ascending order since all (zero)
+// adjacent pairs of integers satisfy the condition that the left integer does not exceed the right integer in value.
+
+
+// TESTS
+//     assert.strictEqual(inAscOrder([1, 2, 4, 7, 19]), true, 'The list of numbers "1, 2, 4, 7, 19" is in ascending order');
+//     assert.strictEqual(inAscOrder([1, 2, 3, 4, 5]), true, 'The list of numbers "1, 2, 3, 4, 5" is in ascending order');
+//     assert.strictEqual(inAscOrder([1, 6, 10, 18, 2, 4, 20]), false, 'The list of numbers "1, 6, 10, 18, 2, 4, 20" is not in ascending order');
+//     assert.strictEqual(inAscOrder([9, 8, 7, 6, 5, 4, 3, 2, 1]), false, 'The list of numbers "9, 8, 7, 6, 5, 4, 3, 2, 1" is in DESCENDING order not ascending order');
+
+
+// SOLUTION
+function inAscOrder(arr) {
+    let copyArr = [...arr].sort((a, b) => a - b)
+    for (let i = 0; i < arr.length; i++)
+        if (arr[i] !== copyArr[i]) {
+            return false
+        }
+    return true
+}
+
+console.log(inAscOrder([1, 2, 4, 7, 19]))
+console.log(inAscOrder([1, 2, 3, 4, 5]))
+console.log(inAscOrder([1, 6, 10, 18, 2, 4, 20]))
+console.log(inAscOrder([9, 8, 7, 6, 5, 4, 3, 2, 1]))
