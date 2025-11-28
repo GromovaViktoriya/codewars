c00d777d78ae92aa9f69e911a5d9e1a90aa28e52
Viktoriya Gromova <rockingirl@mail.ru>
2025-11-28 14:37:33 +0500

    solved oddEvenStringSort 7kyu

--- 7kyu/numbersInOrder.js
+++ 7kyu/numbersInOrder.js
@@ -1,7 +1,8 @@
 // DESCRIPTION
 //Are the numbers in order?
 // In this Kata, your function receives an array of integers as input. Your task is to determine whether the numbers are
-// in ascending order. An array is said to be in ascending order if there are no two adjacent integers where the left integer exceeds the right integer in value.
+// in ascending order. An array is said to be in ascending order if there are no two adjacent integers where the left
+// integer exceeds the right integer in value.
 //
 // For the purposes of this Kata, you may assume that all inputs are valid, i.e. arrays containing only integers.
 //
