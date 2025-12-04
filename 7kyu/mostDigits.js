a6bb78f42ba733752de29593dec11afaa93e8e62
Viktoriya Gromova <rockingirl@mail.ru>
2025-12-04 16:09:56 +0500

    solved mostDigits 7kyu

---
+++ 7kyu/mostDigits.js
@@ -0,0 +1,26 @@
+// DESCRIPTION
+// Find the number with the most digits.
+//
+// If two numbers in the argument array have the same number of digits, return the first one in the array.
+
+
+// TESTS
+// Test.assertEquals(findLongest([1, 10, 100]), 100)
+// Test.assertEquals(findLongest([9000, 8, 800]), 9000)
+// Test.assertEquals(findLongest([8, 900, 500]), 900)
+
+
+// SOLUTION
+function findLongest(array) {
+    let longest = array[0]
+    for (let i = 1; i < array.length; i++) {
+        if (array[i].toString().length > longest.toString().length) {
+            longest = array[i]
+        }
+    }
+    return longest
+}
+
+console.log(findLongest([1, 10, 100]))
+console.log(findLongest([9000, 8, 800]))
+console.log(findLongest([8, 900, 500]))
