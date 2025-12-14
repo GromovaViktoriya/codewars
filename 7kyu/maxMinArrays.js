d3e69356cc8bea8fb5508f148e800c820513f804
Viktoriya Gromova <rockingirl@mail.ru>
2025-12-14 19:54:06 +0500

    solved maxMinArrays 7kyu

--- 7kyu/maxMinArrays.js
+++ 7kyu/maxMinArrays.js
@@ -32,6 +32,8 @@ function solve(arr) {
         }
     }
     return result
+
+    //[...Array(arr.sort((a, b) => a - b).length)].map((_, idx) => idx % 2 ? arr.shift() : arr.pop());
 }
 
 console.log(solve([15, 11, 10, 7, 12]))
