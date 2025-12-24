258db764e338bb03024aa01b34bce8cdc025c8f0
Viktoriya Gromova <rockingirl@mail.ru>
2025-12-24 13:42:45 +0500

    solved phoneNumber 6kyu

---
+++ 6kyu/phoneNumber.js
@@ -0,0 +1,26 @@
+//DESCRIPTION
+//Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers
+// n the form of a phone number.
+//
+// Example
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+// The returned format must be correct in order to complete this challenge.
+//
+// Don't forget the space after the closing parentheses!
+
+
+
+//TESTS
+//     assert.strictEqual(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890");
+//     assert.strictEqual(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), "(111) 111-1111");
+//     assert.strictEqual(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890");
+
+
+// SOLUTION
+function createPhoneNumber(numbers){
+return `(${numbers.slice(0,3).join('')}) ${numbers.slice(3,6).join('')}-${numbers.slice(6,10).join('')}`
+}
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
