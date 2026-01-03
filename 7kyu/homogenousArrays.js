282f4a33ae7a20b1fc540091b75e00fb4526b7f1
Viktoriya Gromova <rockingirl@mail.ru>
2026-01-03 14:35:28 +0500

    solved homogenousArrays 7 kyu

---
+++ 7kyu/homogenousArrays.js
@@ -0,0 +1,30 @@
+// DESCRIPTION
+//Given a two-dimensional array, return a new array which carries over only those arrays from the original, which were
+// not empty and whose items are all of the same type (i.e. homogenous). For simplicity, the arrays inside the array
+// will only contain characters and integers.
+//
+// Example:
+//
+// Given [[1, 5, 4], ['a', 3, 5], ['b'], [], ['1', 2, 3]], your function should return [[1, 5, 4], ['b']].
+//
+// Addendum:
+//
+// Please keep in mind that for this kata, we assume that empty arrays are not homogenous.
+//
+// The resultant arrays should be in the order they were originally in and should not have its values changed.
+//
+// No implicit type casting is allowed. A subarray [1, '2'] would be considered illegal and should be filtered out.
+
+
+// TESTS
+//     assert.deepEqual(filterHomogenous([[1, 5, 4], ['a', 3, 5], ['b'], [], ['1', 2, 3]]), [[1, 5, 4], ['b']]);
+//     assert.deepEqual(filterHomogenous([[123, 234, 432], ['', 'abc'], [''], ['', 1], ['', '1'], []]), [[123, 234, 432], ['', 'abc'], [''], ['', '1']]);
+
+
+// SOLUTION
+function filterHomogenous(arrays) {
+    return arrays.filter(array => array.length > 0 && array.every(item => typeof item === typeof array[0]))
+}
+
+console.log(filterHomogenous([[1, 5, 4], ['a', 3, 5], ['b'], [], ['1', 2, 3]]))
+console.log(filterHomogenous([[123, 234, 432], ['', 'abc'], [''], ['', 1], ['', '1'], []]))
