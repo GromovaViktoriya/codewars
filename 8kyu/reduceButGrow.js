b8a88dba4fa5363241b5e2684a941f4dc78dba91
Viktoriya Gromova <rockingirl@mail.ru>
2026-01-16 11:28:40 +0500

    solved reduceButGrow 8 kyu

---
+++ 8kyu/reduceButGrow.js
@@ -0,0 +1,18 @@
+// DESCRIPTION
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+//
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+// TESTS
+//     assert.strictEqual(grow([1, 2, 3]), 6);
+//     assert.strictEqual(grow([4, 1, 1, 1, 4]), 16);
+//     assert.strictEqual(grow([2, 2, 2, 2, 2, 2]), 64);
+
+// SOLUTION
+function grow(x) {
+  return x.reduce((a, b) => a * b)
+}
+
+console.log(grow([1, 2, 3]))
+console.log(grow([4, 1, 1, 1, 4]))
+console.log(grow([2, 2, 2, 2, 2, 2]))
