d221c63e4e945ef4775ffb04d142c960b6b2bb15
Viktoriya Gromova <rockingirl@mail.ru>
2026-01-19 12:42:27 +0500

    solved removingElements 8 kyu

---
+++ 8kyu/removingElements.js
@@ -0,0 +1,26 @@
+// DESCRIPTION
+//Take an array and remove every second element from the array. Always keep the first element and start removing with
+// the next element.
+//
+// Example:
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+//
+// None of the arrays will be empty, so you don't have to worry about that!
+
+
+// TESTS
+//    assert.deepEqual(removeEveryOther(['Hello', 'Goodbye', 'Hello Again']),['Hello', 'Hello Again']);
+//     assert.deepEqual(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),[1, 3, 5, 7, 9]);
+//     assert.deepEqual(removeEveryOther([[1, 2]]), [[1, 2]]);
+//     assert.deepEqual(removeEveryOther([['Goodbye'], {'Great': 'Job'}]),[['Goodbye']]);
+
+
+// SOLUTION
+function removeEveryOther(arr) {
+    return arr.filter((el, index) => index % 2 === 0);
+}
+
+console.log(removeEveryOther(['Hello', 'Goodbye', 'Hello Again']));
+console.log(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+console.log(removeEveryOther([[1, 2]]));
+console.log(removeEveryOther([['Goodbye'], {'Great': 'Job'}]))
