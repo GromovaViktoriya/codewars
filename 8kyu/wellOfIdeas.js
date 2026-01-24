9b448aa128c01f1275ab3db2255eeaeb741883dc
Viktoriya Gromova <rockingirl@mail.ru>
2026-01-24 15:23:47 +0500

    solved wellOfIdeas 8 kyu

---
+++ 8kyu/wellOfIdeas.js
@@ -0,0 +1,25 @@
+// DESCRIPTION
+//For every good kata idea there seem to be quite a few bad ones!
+//
+// In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or
+// two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas,
+// as is often the case, return 'Fail!'.
+
+
+// TESTS
+//     assert.strictEqual(well(['bad', 'bad', 'bad']), 'Fail!');
+//     assert.strictEqual(well(['good', 'bad', 'bad', 'bad', 'bad']), 'Publish!');
+//     assert.strictEqual(well(['good', 'bad', 'bad', 'bad', 'bad', 'good', 'bad', 'bad', 'good']), 'I smell a series!');
+
+
+// SOLUTION
+function well(x) {
+    let goodIdeasCount = x.filter(idea => idea === 'good').length
+    if (goodIdeasCount === 0) return 'Fail!';
+    if (goodIdeasCount > 0 && goodIdeasCount <= 2) return 'Publish!';
+    if (goodIdeasCount > 2) return 'I smell a series!';
+}
+
+console.log(well(['bad', 'bad', 'bad']))
+console.log(well(['good', 'bad', 'bad', 'bad', 'bad']))
+console.log(well(['good', 'bad', 'bad', 'bad', 'bad', 'good', 'bad', 'bad', 'good']))
