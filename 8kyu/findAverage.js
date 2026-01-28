c8f4715ea66f1a99553ab7cc9c098d0d3b0e766c
Viktoriya Gromova <rockingirl@mail.ru>
2026-01-28 12:53:09 +0500

    solved findAverage 8 kyu

--- 8kyu/findAverage.js
+++ 8kyu/findAverage.js
@@ -24,11 +24,13 @@
 
 // SOLUTION
 function findAverage(nums) {
-    let sum = 0
-    for(let i = 0; i < nums.length; i++){
-        sum += nums[i]
-    }
-    return sum / nums.length
+    // let sum = 0
+    // for(let i = 0; i < nums.length; i++){
+    //     sum += nums[i]
+    // }
+    // return sum / nums.length
+
+    return nums.reduce((a, b) => a + b) / nums.length
 }
 
 console.log(findAverage([1]))
