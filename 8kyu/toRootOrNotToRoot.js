d48fcb90ab0cde30da83773c1ae0da5afa1ff90f
Viktoriya Gromova <rockingirl@mail.ru>
2026-01-29 12:44:42 +0500

    solved toRootOrNotToRoot 8 kyu

---
+++ 8kyu/toRootOrNotToRoot.js
@@ -0,0 +1,36 @@
+// DESCRIPTION
+// Write a method, that will get an integer array as parameter and will process every number from this array.
+//
+// Return a new array with processing every number of the input-array like this:
+//
+// If the number has an integer square root, take this, otherwise square the number.
+//
+// Example
+// [4,3,9,7,2,1] -> [2,9,3,49,4,1]
+// Notes
+// The input array will always contain only positive numbers, and will never be empty or null.
+
+
+// TESTS
+//     var input = [ 4, 3, 9, 7, 2, 1 ];
+//     var expected = [ 2, 9, 3, 49, 4, 1 ];
+//     Test.assertSimilar(squareOrSquareRoot(input), expected);
+//
+//     input = [ 100, 101, 5, 5, 1, 1 ];
+//     expected = [ 10, 10201, 25, 25, 1, 1 ];
+//     Test.assertSimilar(squareOrSquareRoot(input), expected);
+//
+//     input = [ 1, 2, 3, 4, 5, 6 ];
+//     expected = [ 1, 4, 9, 2, 25, 36 ];
+//     Test.assertSimilar(squareOrSquareRoot(input), expected);
+
+
+// SOLUTION
+function squareOrSquareRoot(array) {
+    // return array.map(num => Math.sqrt(num) % 1 === 0 ? Math.sqrt(num) : num ** 2)
+    return array.map(num => Number.isInteger(Math.sqrt(num)) ? Math.sqrt(num) : num ** 2)
+}
+
+console.log(squareOrSquareRoot([ 4, 3, 9, 7, 2, 1 ]))
+console.log(squareOrSquareRoot([ 100, 101, 5, 5, 1, 1 ]))
+console.log(squareOrSquareRoot([ 1, 2, 3, 4, 5, 6 ]))
