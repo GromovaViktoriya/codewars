274cbcfd728ee9456e707afacabcb93408b5f4a4
Viktoriya Gromova <rockingirl@mail.ru>
2026-02-04 14:00:34 +0500

    solved oldestYoungestDifference 8 kyu

---
+++ 8kyu/oldestYoungestDifference.js
@@ -0,0 +1,22 @@
+// DESCRIPTION
+//At the annual family gathering, the family likes to find the oldest living family member’s age and the youngest family
+// member’s age and calculate the difference between them.
+//
+// You will be given an array of all the family members' ages, in any order. The ages will be given in whole numbers,
+// so a baby of 5 months, will have an ascribed ‘age’ of 0. Return a new array (a tuple in Python) with [youngest age,
+// oldest age, difference between the youngest and oldest age].
+
+// TESTS
+//   assert.deepEqual(differenceInAges([82, 15, 6, 38, 35]), [6, 82, 76]);
+//   assert.deepEqual(differenceInAges([57, 99, 14, 32]), [14, 99, 85]);
+
+// SOLUTION
+function differenceInAges(ages) {
+    let max = Math.max(...ages)
+    let min = Math.min(...ages)
+    let diff = max - min
+    return [min, max, diff];
+}
+
+console.log(differenceInAges([82, 15, 6, 38, 35]))
+console.log(differenceInAges([57, 99, 14, 32]))
