7aa17665cf15b92c132c03cdc3ae25e895e67ecd
Viktoriya Gromova <rockingirl@mail.ru>
2026-02-13 16:00:15 +0500

    solved mergingSortedIntegerArrays 8 kyu

---
+++ 8kyu/mergingSortedIntegerArrays.js
@@ -0,0 +1,17 @@
+// DESCRIPTION
+// Write a function that merges two sorted arrays into a single one. The arrays only contain integers. Also, the final
+// outcome must be sorted and not have any duplicate.
+
+
+// TESTS
+//     assert.deepEqual(mergeArrays([1, 3, 5], [2, 4, 6]), [1, 2, 3, 4, 5, 6]);
+//     assert.deepEqual(mergeArrays([2, 4, 8], [2, 4, 6]), [2, 4, 6, 8]);
+
+
+// SOLUTION
+function mergeArrays(a, b) {
+  return [...new Set([...a, ...b])].sort((a, b) => a - b)
+}
+
+console.log(mergeArrays([1, 3, 5], [2, 4, 6]))
+console.log(mergeArrays([2, 4, 8], [2, 4, 6]))
