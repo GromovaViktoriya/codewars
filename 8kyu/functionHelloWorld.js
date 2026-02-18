b1f0abea8acd30b84ff2ec6ffb425d36da91be3b
Viktoriya Gromova <rockingirl@mail.ru>
2026-02-18 12:10:53 +0500

    solved functionHelloWorld 8kyu

---
+++ 8kyu/functionHelloWorld.js
@@ -0,0 +1,16 @@
+// DESCRIPTION
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+
+// TESTS
+//  it("Is it a function?", function() {
+//     assert.strictEqual(typeof greet, "function", "greet should be a function");
+//   });
+//   it("Correct return-value?", function() {
+//     assert.strictEqual(greet(), "hello world!");
+
+
+// SOLUTION
+let greet = function(){
+    return 'hello world!'
+}
