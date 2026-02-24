8eae88ceab713cc7011a57f3cde0bec7ff498f68
Viktoriya Gromova <rockingirl@mail.ru>
2026-02-24 16:26:20 +0500

    solved simpleMultiplication 8kyu

--- 8kyu/simpleMultiplication.js
+++ 8kyu/simpleMultiplication.js
@@ -3,7 +3,7 @@
 
 
 // TESTS
-//    assert.strictEqual(simpleMultiplication(2),16,'Should return given argument times eight...')
+//     assert.strictEqual(simpleMultiplication(2),16,'Should return given argument times eight...')
 //     assert.strictEqual(simpleMultiplication(1),9, 'Should return given argument times nine...')
 //     assert.strictEqual(simpleMultiplication(8),64,'Should return given argument times eight...')
 //     assert.strictEqual(simpleMultiplication(4),32,'Should return given argument times eight...')
