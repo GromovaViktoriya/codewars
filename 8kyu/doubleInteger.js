8c738502c4f6b98522f72698e6ba6cd04a6ad828
Viktoriya Gromova <rockingirl@mail.ru>
2026-03-03 18:09:18 +0500

    solved doubleInteger 8kyu

---
+++ 8kyu/doubleInteger.js
@@ -0,0 +1,10 @@
+// DESCRIPTION
+//Code as fast as you can! You need to double the integer and return it.
+
+// TESTS
+//Test.assertEquals(doubleInteger(2), 4);
+
+// SOLUTION
+function doubleInteger(i) {
+    return i*i
+}
