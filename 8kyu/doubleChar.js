cd9e79f9fc10d65a44373c1dc59258d556b8a11a
Viktoriya Gromova <rockingirl@mail.ru>
2026-03-03 18:21:55 +0500

    solved doubleChar 8kyu

---
+++ 8kyu/doubleChar.js
@@ -0,0 +1,28 @@
+// DESCRIPTION
+//Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+//
+// Examples (Input -> Output):
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+
+// TESTS
+//    Test.assertEquals(doubleChar("abcd"), "aabbccdd");
+//     Test.assertEquals(doubleChar("Adidas"), "AAddiiddaass");
+//     Test.assertEquals(doubleChar("1337"), "11333377");
+//     Test.assertEquals(doubleChar("illuminati"), "iilllluummiinnaattii");
+//     Test.assertEquals(doubleChar("123456"), "112233445566");
+//     Test.assertEquals(doubleChar("%^&*("), "%%^^&&**((");
+
+
+// SOLUTION
+function doubleChar(str) {
+    return str.split('').map(char => char + char).join('')
+}
+
+console.log(doubleChar("abcd"));
+console.log(doubleChar("Adidas"));
+console.log(doubleChar("1337"));
+console.log(doubleChar("illuminati"));
+console.log(doubleChar("123456"));
+console.log(doubleChar("%^&*("));
