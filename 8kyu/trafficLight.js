c1116f5abfc55b7bccd5915337eb9683ed4feb7e
Viktoriya Gromova <rockingirl@mail.ru>
2026-03-06 16:20:28 +0500

    solved trafficLight 8kyu

---
+++ 8kyu/trafficLight.js
@@ -0,0 +1,29 @@
+// DESCRIPTION
+//You're writing code to control your town's traffic lights. You need a function to handle each change from green, to
+// yellow, to red, and then to green again.
+//
+// Complete the function that takes a string as an argument representing the current state of the light and returns a
+// string representing the state the light should change to.
+//
+// For example, when the input is green, output should be yellow.
+
+// TESTS
+//     assert.strictEqual(updateLight("green"), "yellow");
+//     assert.strictEqual(updateLight("yellow"), "red");
+//     assert.strictEqual(updateLight("red"), "green");
+
+// SOLUTION
+function updateLight(current) {
+    switch (current) {
+        case 'green':
+            return 'yellow';
+        case 'yellow':
+            return 'red';
+        default:
+            return 'green';
+    }
+}
+
+console.log(updateLight("green"))
+console.log(updateLight("yellow"))
+console.log(updateLight("red"))
