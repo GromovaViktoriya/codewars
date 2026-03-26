cb7a753d42febb46830217631ad4d6c7e5823da7
Viktoriya Gromova <rockingirl@mail.ru>
2026-03-26 19:29:52 +0500

    solved debugSayHello 8kyu

--- 8kyu/debugSayHello.js
+++ 8kyu/debugSayHello.js
@@ -21,4 +21,4 @@ function sayHello(name) {
 console.log(sayHello('Mr. Spock'))
 console.log(sayHello('Captain Kirk'))
 console.log(sayHello('Liutenant Uhura'))
-console.log(sayHello('Dr. McCoy'))
+console.log(sayHello('Dr.McCoy'))
