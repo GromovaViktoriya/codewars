4116d879bc64253fad4a236e98d791d7cb432960
Viktoriya Gromova <rockingirl@mail.ru>
2026-03-31 10:54:56 +0500

    solved squaringAnArgument 8kyu

---
+++ 8kyu/squaringAnArgument.js
@@ -0,0 +1,10 @@
+// DESCRIPTION
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// TESTS
+//    assert.strictEqual(square(3), 9);
+
+// SOLUTION
+const square = (x) => x * x;
+
+console.log(square(3))
