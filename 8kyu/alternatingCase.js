3c0896aaf453a7b0ad4aa31c99278726f617ebe0
Viktoriya Gromova <rockingirl@mail.ru>
2026-04-03 09:40:11 +0500

    solved reverseListOrder 8kyu

--- 8kyu/alternatingCase.js
+++ 8kyu/alternatingCase.js
@@ -1,5 +1,5 @@
 // DESCRIPTION
-//Define String.prototype.toAlternatingCase (or a similar function/method such as
+// Define String.prototype.toAlternatingCase (or a similar function/method such as
 // to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution
 // for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase.
 // For example:
