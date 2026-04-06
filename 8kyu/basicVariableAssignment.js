6a23d9829316e737df2dafa49f65c99d2d63b64b
Viktoriya Gromova <rockingirl@mail.ru>
2026-04-06 21:20:56 +0500

    corrected basicVariableAssignment 8kyu

---
+++ 8kyu/basicVariableAssignment.js
@@ -0,0 +1,12 @@
+// DESCRIPTION
+// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+// TESTS
+//    assert.strictEqual(name, "codewa.rs");
+
+// SOLUTION
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
+
+console.log(name)
