9ad9161b4c058c2be5e863b164f5d165fd5bcc7c
Viktoriya Gromova <rockingirl@mail.ru>
2026-04-08 16:14:51 +0500

    solved nameShuffler 8kyu

---
+++ 8kyu/nameShuffler.js
@@ -0,0 +1,20 @@
+// DESCRIPTION
+// Write a function that returns a string in which firstname is swapped with last name.
+//
+// Example(Input --> Output)
+//
+// "john McClane" --> "McClane john"
+
+// TESTS
+//     assert.strictEqual(nameShuffler('john McClane'),'McClane john');
+//     assert.strictEqual(nameShuffler('Mary jeggins'),'jeggins Mary')
+//     assert.strictEqual(nameShuffler('tom jerry'),'jerry tom')
+
+// SOLUTION
+function nameShuffler(str){
+return str.split(' ').reverse().join(' ');
+}
+
+console.log(nameShuffler('john McClane'))
+console.log(nameShuffler('Mary jeggins'))
+console.log(nameShuffler('tom jerry'))
