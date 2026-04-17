d46a6939664c7670f4306bcbdd02f0e3beaf3ce5
Viktoriya Gromova <rockingirl@mail.ru>
2026-04-17 16:37:54 +0500

    solved isThisMyTail 8kyu

--- 8kyu/removeExclamationMark.js
+++ 8kyu/removeExclamationMark.js
@@ -1,5 +1,4 @@
 // DESCRIPTION
-//Description:
 // Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is
 // always a string, no need to verify it.
 
