288b1fc1c603367c162354731b5bdbcd1b553b70
Viktoriya Gromova <rockingirl@mail.ru>
2026-05-19 21:05:38 +0500

    solved powerOfTwo 8kyu

--- 8kyu/powerOfTwo.js
+++ 8kyu/powerOfTwo.js
@@ -14,13 +14,13 @@
 
 // SOLUTION
 function powersOfTwo(n){
-  let res = [];
-  for (let i = 0; i <= n; i++) {
-      res.push(Math.pow(i, 2));
-  }
-  return res;
+    let res = [];
+    for (let i = 0; i <= n; i++) {
+        res.push(Math.pow(2, i))
+    }
+    return res;
 }
 
 console.log(powersOfTwo(0))
 console.log(powersOfTwo(1))
-console.log(powersOfTwo(2))
+console.log(powersOfTwo(4))
