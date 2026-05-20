f29fecf1d8452c5d94dc3a9f01b25830cdd6e275
Viktoriya Gromova <rockingirl@mail.ru>
2026-05-20 16:33:26 +0500

    solved replaceAllVowel 8kyu

---
+++ 8kyu/replaceAllVowel.js
@@ -0,0 +1,27 @@
+// DESCRIPTION
+// Description:
+// Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+//
+// Examples
+// "Hi!" --> "H!!"
+// "!Hi! Hi!" --> "!H!! H!!"
+// "aeiou" --> "!!!!!"
+// "ABCDE" --> "!BCD!"
+
+
+// TESTS
+//         doTest("Hi!", "H!!");
+//         doTest("!Hi! Hi!", "!H!! H!!");
+//         doTest("aeiou", "!!!!!");
+//         doTest("ABCDE", "!BCD!");
+
+
+// SOLUTION
+function replace(s) {
+    return s.replaceAll(/[aeiou]/gi, '!');
+}
+
+console.log(replace("Hi!"))
+console.log(replace("!Hi! Hi!"))
+console.log(replace("aeiou"))
+console.log(replace("ABCDE"))
