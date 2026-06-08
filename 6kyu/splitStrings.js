4e516af58173975ef92ed61d2fcbff5405c6c88e
Viktoriya Gromova <rockingirl@mail.ru>
2026-06-08 11:58:02 +0500

    solved splitStrings 6kyu

---
+++ 6kyu/splitStrings.js
@@ -0,0 +1,32 @@
+// DESCRIPTION
+// Complete the solution so that it splits the string into strings of two characters in a list/array (depending on the
+// language you use). If the string contains an odd number of characters then it should replace the missing second
+// character of the final pair with an underscore ('_').
+//
+// Examples:
+//
+// * 'abc' =>  ['ab', 'c_']
+// * 'abcdef' => ['ab', 'cd', 'ef']
+
+
+// TESTS
+//     assert.deepEqual(solution("abcdef"), ["ab", "cd", "ef"]);
+//     assert.deepEqual(solution("abcdefg"), ["ab", "cd", "ef", "g_"]);
+//     assert.deepEqual(solution(""), []);
+
+
+// SOLUTION
+function solution(str) {
+    let result = [];
+    for (let i = 0; i < str.length; i += 2) {
+    result.push(str.slice(i, i + 2));
+    }
+    if (str.length % 2 !== 0) {
+        result[result.length - 1] += "_"
+    }
+    return result;
+}
+
+console.log(solution("abcdef"))
+console.log(solution("abcdefg"))
+console.log(solution(""))
