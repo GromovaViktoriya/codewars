35557cf2d2573ab288d74e33989507be85be342b
Viktoriya Gromova <rockingirl@mail.ru>
2026-06-15 13:50:28 +0500

    solved converStringToCamelCase 6kyu

--- 6kyu/converStringToCamelCase.js
+++ 6kyu/converStringToCamelCase.js
@@ -20,12 +20,8 @@
 
 // SOLUTION
 function toCamelCase(str) {
-    return str.split(/[-_]/).map((word, index) => {
-        if (index === 0) {
-            return word
-        }
-        return word[0].toUpperCase() + word.slice(1)
-    }).join('')
+    return str.split(/[-_]/).map((word, index) => index === 0? word : word[0].toUpperCase() + word.slice(1)).join('')
+    // return str.replace(/[-_](.)/g, (_match, letter) => letter.toUpperCase());
 }
 
 console.log(toCamelCase(''))
