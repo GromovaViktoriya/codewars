c6fcd9333ee271b922c46ae672985f814f3becff
Viktoriya Gromova <rockingirl@mail.ru>
2026-06-24 12:26:09 +0500

    solved groupAnagrams 6kyu

---
+++ 6kyu/groupAnagrams.js
@@ -0,0 +1,40 @@
+// DESCRIPTION
+// Your job is to group the words in anagrams.
+//
+// What is an anagram ?
+// star and tsar are anagram of each other because you can rearrange the letters for star to obtain tsar.
+//
+// Example
+// A typical test could be :
+//
+// // input
+// ["tsar", "rat", "tar", "star", "tars", "cheese"]
+//
+// // output
+// [
+//   ["tsar", "star", "tars"],
+//   ["rat", "tar"],
+//   ["cheese"]
+// ]
+// Hvae unf !
+//
+// I'd advise you to find an efficient way for grouping the words in anagrams otherwise you'll probably won't pass
+// the heavy superhero test cases
+
+
+// TESTS
+//   doTest(["rat", "tar", "star"], [["rat", "tar"], ["star"]]);
+
+
+// SOLUTION
+function groupAnagrams(words) {
+let anagramGroups = {}
+    for (let word of words) {
+        let sortedWord = word.split('').sort().join('')
+        if (!(sortedWord in anagramGroups)) anagramGroups[sortedWord] = []
+        anagramGroups[sortedWord].push(word)
+    }
+    return Object.values(anagramGroups)
+}
+
+console.log(groupAnagrams(["tsar", "rat", "tar", "star", "tars", "cheese"]))
