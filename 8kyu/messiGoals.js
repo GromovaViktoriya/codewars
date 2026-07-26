e2ae4515911365bacaa1cffa6cc63a1a487d8653
Viktoriya Gromova <rockingirl@mail.ru>
2026-07-26 21:10:52 +0500

    solved messiGoals 8kyu

---
+++ 8kyu/messiGoals.js
@@ -0,0 +1,23 @@
+// DESCRIPTION
+// Messi goals function
+// Messi is a soccer player with goals in three leagues:
+//
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+//
+// Note: the input will always be valid.
+//
+// For example:
+//
+// 5, 10, 2  -->  17
+
+// TESTS
+// Test.assertEquals(goals(0,0,0), 0)
+// Test.assertEquals(goals(43, 10, 5), 58)
+
+// SOLUTION
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals+copaDelReyGoals+championsLeagueGoals
+}
