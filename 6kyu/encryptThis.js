afa2f91c730ca4862b3efe72b2f8f8f7204f4387
Viktoriya Gromova <rockingirl@mail.ru>
2026-08-03 14:22:39 +0500

    solved encryptThis 6kyu

---
+++ 6kyu/encryptThis.js
@@ -0,0 +1,35 @@
+// DESCRIPTION
+// Description:
+// Encrypt this!
+//
+// You want to create secret messages which can be deciphered by the Decipher this! kata. Here are the conditions:
+//
+// Your message is a string containing space separated words.
+// You need to encrypt each word in the message using the following rules:
+// The first letter must be converted to its ASCII code.
+// The second letter must be switched with the last letter
+// Keepin' it simple: There are no special characters in the input.
+// Examples:
+// encryptThis("Hello") === "72olle"
+// encryptThis("good") === "103doo"
+// encryptThis("hello world") === "104olle 119drlo"
+
+
+// TESTS
+//     assert.strictEqual(encryptThis("A wise old owl lived in an oak"), "65 119esi 111dl 111lw 108dvei 105n 97n 111ka");
+//     assert.strictEqual(encryptThis("The more he saw the less he spoke"), "84eh 109ero 104e 115wa 116eh 108sse 104e 115eokp");
+//     assert.strictEqual(encryptThis("The less he spoke the more he heard"), "84eh 108sse 104e 115eokp 116eh 109ero 104e 104dare");
+//     assert.strictEqual(encryptThis("Why can we not all be like that wise old bird"), "87yh 99na 119e 110to 97ll 98e 108eki 116tah 119esi 111dl 98dri");
+//     assert.strictEqual(encryptThis("Thank you Piotr for all your help"), "84kanh 121uo 80roti 102ro 97ll 121ruo 104ple");
+
+
+// SOLUTION
+
+let encryptThis = function (text) {
+    return text.split(" ").map(word => word.length === 1
+        ? word.charCodeAt(0)
+        : word.length === 2
+            ? word[0].charCodeAt(0) + word.slice(word.length-1)
+            : word[0].charCodeAt(0) + word.slice(word.length-1) + word.slice(2, word.length-1) + word[1]).join(" ")
+}
+console.log(encryptThis('A wise old owl lived in an oak'))
