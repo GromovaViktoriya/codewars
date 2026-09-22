02227be2501193b80da66febe3dc1e6f06eaace2
Viktoriya Gromova <rockingirl@mail.ru>
2026-09-22 17:00:03 +0500

    solved 6 kyu alphabetized

---
+++ 6kyu/alphabetized.js
@@ -0,0 +1,23 @@
+// DESCRIPTION
+// The alphabetized kata
+// Re-order the characters of a string, so that they are concatenated into a new string in
+// "case-insensitively-alphabetical-order-of-appearance" order. Whitespace and punctuation shall simply be removed!
+//
+// The input is restricted to contain no numerals and only words containing the english alphabet letters.
+//
+// Example:
+//
+// alphabetized("The Holy Bible") // "BbeehHilloTy"
+
+// TESTS
+//   Test.assertEquals(alphabetized('The Holy Bible'), 'BbeehHilloTy')
+
+//SOLUTION
+function alphabetized(s) {
+    return s.split(' ').join('').split('')
+        .filter(char => /^[a-zA-Z]+$/.test(char))
+        .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase())).join('')
+}
+
+console.log(alphabetized("The Holy Bible"))
+console.log(alphabetized("The Holy, Bible."))
